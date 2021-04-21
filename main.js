9934f8900e734eaa8e58141f9a752ec9d577b8c6
axdi0n <acorinia@fcpn.edu.bo>
2021-04-20 23:18:51 -0400

    Updates

---
+++ main.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.contenido',{delay:500});
+ScrollReveal().reveal('.contenido', {delay:500});
+ScrollReveal().reveal('.cards-banner-two',{delay:500});
